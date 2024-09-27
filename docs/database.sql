32f9d1a1be766a6eb0ae510e8e6427fa9368bac0
Lin <2314445395@qq.com>
2024-09-27 17:32:27 +0800

    更新数据库，增添民宿、省、市、区、评分和评价表

--- docs/database.sql
+++ docs/database.sql
@@ -13,3 +13,65 @@ CREATE TABLE users
     created_at TIMESTAMP                             DEFAULT CURRENT_TIMESTAMP,                            -- 用户账户的创建时间
     updated_at TIMESTAMP                             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 用户账户的最后更新时间（如用户信息的修改）
 );
+
+-- 民宿信息表
+CREATE TABLE listings
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,      -- 民宿ID
+    district_id INT,                                 -- 区ID，外键
+    title       VARCHAR(100) NOT NULL,               -- 民宿标题
+    description TEXT,                                -- 民宿描述
+    price       INT,                                 -- 价格
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 创建时间
+    location    VARCHAR(50),                         -- 地理位置
+    FOREIGN KEY (district_id) REFERENCES districts (id) ON DELETE CASCADE
+);
+
+-- 省
+CREATE TABLE provinces
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+-- 市
+CREATE TABLE cities
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL,
+    province_id INT,
+    FOREIGN KEY (province_id) REFERENCES provinces (id)
+);
+
+-- 区
+CREATE TABLE districts
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT,
+    name    VARCHAR(50) NOT NULL,
+    city_id INT,
+    FOREIGN KEY (city_id) REFERENCES cities (id)
+);
+
+-- 评分表
+CREATE TABLE mark
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    user_id    INT NOT NULL,
+    listing_id INT NOT NULL,
+    score      INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (listing_id) REFERENCES listings (id)
+);
+
+-- 评价表
+CREATE TABLE evaluate
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    user_id     INT  NOT NULL,
+    listing_id  INT  NOT NULL,
+    mark_id     INT  NOT NULL,
+    description TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (listing_id) REFERENCES listings (id),
+    FOREIGN KEY (mark_id) REFERENCES mark (id)
+);
